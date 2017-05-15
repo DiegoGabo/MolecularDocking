df2d90a83f40c14a02c46aa914e53a7ca5de290d
Danilo Labanca <danilo.labanca@mail.polimi.it>
2017-05-15 16:52:35 +0200

    complete parser

--- parser.cpp
+++ parser.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -36,35 +37,140 @@ vector<string> split_line(string& line)
     return elements;
 }
 
+class Atom
+{
+    float x, y, z;
+
+public:
+    
+    Atom(float x, float y, float z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    
+    float getX()
+    {
+        return x;
+    }
+    
+    float getY()
+    {
+        return y;
+    }
+    
+    float getZ()
+    {
+        return z;
+    }
+    
+    string toString()
+    {
+        return "x = " + std::to_string(x) + " y = " + std::to_string(y) + " z = " + std::to_string(z);
+    }
+    
+};
+
+class Molecule
+{
+    std::vector<Atom> atoms;
+    std::vector<std::list<int>> links;
+    
+    
+public:
+    
+    void addAtom(Atom atom)
+    {
+        atoms.push_back(atom);
+        std::list<int> link;
+        links.push_back(link);
+    }
+    
+    void setEdge(int src, int dest)
+    {
+        links.at(src).push_back(dest);
+    }
+    
+    
+    string toString()
+    {
+        //creates a string with the list of atoms
+        string molecule = "The molecule has the following atoms";
+        for (Atom atom : atoms)
+        {
+            molecule += "\n";
+            molecule += atom.toString();
+        }
+        
+        //creates a string with the structure of the graph
+        molecule += "\n\nGraph structure:";
+        int atom = 0;
+        for (std::list<int> link : links)
+        {
+            molecule += "\nAtom:  ";
+            molecule += std::to_string(atom);
+            molecule += " linked to: ";
+            for (std::list<int>::iterator it = link.begin(); it != link.end(); ++it)
+            {
+                molecule += std::to_string(*it);
+                molecule += ", ";
+            }
+            atom++;
+        }
+        return molecule;
+    }
+};
 
 
 int main()
 {
     fstream ligand("ligand_dataset.txt");
-    string line;
-    vector<string> elements;
-    vector<Atom> atoms;
-    Molecule molecules;
+    
     
     if( ligand.is_open())
     {
         cout << "File ok\n";
         
+        string line;
+        vector<string> elements;
+        Molecule molecule;
+        int atoms_index = 0;
+        
+        int count_atoms = 0;    //just for testing
+        
         while (getline (ligand, line))
         {
-            int atoms_index = 0;
             elements = split_line(line);
             
-            if(elements[0].compare(="ATOM"))
+            if(elements[0].compare("ATOM") == 0)
             {
-                Atom new_atom (elements[6], elements[7], elements[8]);
-                atoms.push_back(new_atom);
+                cout << "I find a new atom #" << ++count_atoms << '\n';
+                
+                float x = stof(elements[6]);
+                float y = stof(elements[7]);
+                float z = stof(elements[8]);
+                
+                Atom new_atom (x, y, z);
+                
+                cout << "It's composition " << new_atom.toString() << '\n';
+                
+                molecule.addAtom(new_atom);
             }
             else
             {
+                cout << "I'm connecting some atoms\n";
                 
+                for (int i = 1; i < elements.size(); i++)
+                {
+                    cout << "Edge: " << atoms_index << "->" << elements[i] << '\n';
+                    molecule.setEdge(atoms_index, stoi(elements[i]));
+                }
                 
+                atoms_index++;
             }
+            
+            cout << '\n';
             
 //            for (int i = 0; i<elements.size(); i++)
 //                cout << i << ": " << elements.at(i) << '\n';
@@ -73,6 +179,9 @@ int main()
         }
         
         ligand.close();
+        
+        cout << "The resultant molecule:\n";
+        cout << molecule.toString();
     }
     else cout << "Unable to open the file\n";
     
