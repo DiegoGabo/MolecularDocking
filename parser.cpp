22581e590b80621ae1e67b54bf67385b4cf84b6f
Danilo Labanca <danilo.labanca@mail.polimi.it>
2017-05-16 11:26:24 +0200

    complete version parser

--- parser.cpp
+++ parser.cpp
@@ -15,35 +15,11 @@
 
 using namespace std;
 
-const string MOLECULE_BEGIN = "@<TRIPOS> MOLECULE";
-const string MOLECULE_ATOMS = "@<TRIPOS> ATOM";
-const string MOLECULE_BONDS = "@<TRIPOS> BOND";
+const string MOLECULE_BEGIN ("@<TRIPOS>MOLECULE");
+const string MOLECULE_ATOMS ("@<TRIPOS>ATOM");
+const string MOLECULE_BONDS ("@<TRIPOS>BOND");
+const string MOLECULE_NULL ("EMPTY MOLECULE");
 
-
-vector<string> splitLine(string& line)
-{
-    vector<string> elements;
-    string temp = "";
-    
-    for (int i = 0; i < line.size(); i++)
-    {
-        if (line.at(i) != ' ')
-            temp.append(line.substr(i,1));
-        else
-        {
-            if (temp.compare("") != 0)
-            {
-                elements.push_back(temp);
-                temp.clear();
-            }
-        }
-    }
-    
-    if (temp.compare("") != 0)
-        elements.push_back(temp);
-    
-    return elements;
-}
 
 class Atom
 {
@@ -101,6 +77,11 @@ public:
         this->name = name;
     }
     
+    string getName()
+    {
+        return name;
+    }
+    
     void addAtom(Atom atom)
     {
         atoms.push_back(atom);
@@ -116,6 +97,9 @@ public:
     
     string toString()
     {
+        
+        cout << "Molecule name: " << name << '\n';
+        
         //creates a string with the list of atoms
         string molecule = "The molecule has the following atoms";
         for (Atom atom : atoms)
@@ -143,70 +127,102 @@ public:
     }
 };
 
+vector<string> splitLine(string& line)
+{
+    vector<string> elements;
+    string temp = "";
+    
+    for (int i = 0; i < line.size(); i++)
+    {
+        if (line.at(i) != ' ')
+            temp.append(line.substr(i,1));
+        else
+        {
+            if (temp.compare("") != 0)
+            {
+                elements.push_back(temp);
+                temp.clear();
+            }
+        }
+    }
+    
+    if (temp.compare("") != 0)
+        elements.push_back(temp);
+    
+    return elements;
+    
+}
+
 
 int main()
 {
     
-    fstream ligand("ace_ligands.mol2");
+    fstream ligands("ace_ligands.mol2");
     
-    if( ligand.is_open())
+    if( ligands.is_open())
     {
-        cout << "File ok\n";
+        cout << "File ok\n\n\n";
         
         string line;
         vector<string> text_elements;
-        Molecule* temp_molecule = new Molecule("EMPTY MOLECULE");
+        
+        Molecule* temp_molecule = new Molecule(MOLECULE_NULL);
+        vector<Molecule> molecules;
+        
         bool name_flag = false;
         bool atoms_flag = false;
         bool bonds_flag = false;
         
-        int atoms_index = 0;    //just for testing
-        int count_molecule = 0; //just for testing
-        int count_atoms = 0;    //just for testing
-        
-        while (getline (ligand, line))
+        while (getline (ligands, line))
         {
             text_elements = splitLine(line);
             
+            if (text_elements.empty())
+                continue;
+            
             if (text_elements[0].compare(MOLECULE_BEGIN) == 0)
             {
-                cout << "I find a new molecule, #" << ++count_molecule << '\n';
+                //if the first element we meet is @<TRIPOS> MOLECULE this means that we are at the begin of the description of a molecule
+                //in this case we set the relative flag to true and, if it's possible, we insert the new molecule in the vector and instanziates a new empty molecule.
                 
                 name_flag = true;
                 atoms_flag = false;
                 bonds_flag = false;
                 
+                if (temp_molecule->getName().compare(MOLECULE_NULL) != 0)
+                {
+                    molecules.push_back(*temp_molecule);
+                    temp_molecule = new Molecule(MOLECULE_NULL);
+                }
             }
             else if (text_elements[0].compare(MOLECULE_ATOMS) == 0)
             {
-                cout << "These are its atoms\n";
+                cout << "These are its atoms\n";    //just for testing
                 
+                //if the first element we meet is @<TRIPOS> ATOM means that the next lines describes the atoms of the molecule
                 name_flag = false;
                 atoms_flag = true;
                 bonds_flag = false;
-                
-//
-//                for (int i = 1; i < elements.size(); i++)
-//                {
-//                    cout << "Edge: " << atoms_index << "->" << elements[i] << '\n';
-//                    molecule.setEdge(atoms_index, stoi(elements[i]));
-//                }
-//                
-//                atoms_index++;
             }
             else if (text_elements[0].compare(MOLECULE_BONDS) == 0)
             {
-                cout << "These are its bonds\n";
+                cout << "These are its bonds\n";    //just for testing
                 
+                //if the first element we meet is @<TRIPOS> BONDS means that the next lines describes the bonds between the atoms of the molecule
                 name_flag = false;
                 atoms_flag = false;
                 bonds_flag = true;
             }
-            else
+            else    //we follow this branch if we are exctracting information about the name, the atoms or the bonds of the molecule
             {
-                if (name_flag && !atoms_flag && !bonds_flag)
+                if (!name_flag && !atoms_flag && !bonds_flag)
                 {
-                    temp_molecule = new Molecule (text_elements[0]);
+                    continue;
+                }
+                else if (name_flag && !atoms_flag && !bonds_flag)
+                {
+                    temp_molecule->setName(text_elements[0]);
+                    name_flag = false;
                 }
                 else if (!name_flag && atoms_flag && !bonds_flag)
                 {
@@ -216,7 +232,7 @@ int main()
                     
                     temp_molecule->addAtom(Atom (x, y, z));
                 }
-                else if (!name_flag && !atoms_flag && !bonds_flag)
+                else if (!name_flag && !atoms_flag && bonds_flag)
                 {
                     int source = stoi(text_elements[1]);
                     int destination = stoi(text_elements[2]);
@@ -227,15 +243,28 @@ int main()
             
             cout << '\n';
             
+//            understand how the lines int the mol2 file are made up
+//
 //            for (int i = 0; i<elements.size(); i++)
 //                cout << i << ": " << elements.at(i) << '\n';
 //            
 //            cout << '\n';
         }
         
-        ligand.close();
+        ligands.close();
+        
+        cout << "\n\nThese are the molecules:\n\n";
+        
+        for (int i = 0; i < molecules.size(); i++)
+        {
+            cout << molecules[i].toString() << "\n\n";
+            cout << "-----------------------------------------------------------------\n";
+        }
+        
+            
     }
-    else cout << "Unable to open the file\n";
+    else
+        cout << "Unable to open the file\n";
     
     return 0;
 }
